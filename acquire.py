922a900dfd52d01c591efa0a51051f982a94bd14
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-02-28 15:30:08 -0600

    Added documentation

--- acquire.py
+++ acquire.py
@@ -7,18 +7,23 @@ from env import get_db_url
 # File names and database names for each dataset acquired through the 
 # following functions.
 
-titanic_file = 'titanic.csv'
-titanic_db = 'titanic_db'
+_titanic_file = 'titanic.csv'
+_titanic_db = 'titanic_db'
 
-iris_file = 'iris.csv'
-iris_db = 'iris_db'
+_iris_file = 'iris.csv'
+_iris_db = 'iris_db'
 
-telco_file = 'telco.csv'
-telco_db = 'telco_churn'
+_telco_file = 'telco.csv'
+_telco_db = 'telco_churn'
 
-
+##########
 
 def _get_titanic_sql() -> str:
+    '''
+    Returns the SQL code required to retrieve the titanic dataset
+    from the MySQL database.
+    '''
+
     return '''
         SELECT * FROM passengers;
     '''
@@ -31,16 +36,34 @@ def get_titanic_data(use_cache = True) -> pd.core.frame.DataFrame:
     exist the data will be cached in that file inside the current
     working directory. Otherwise, the data will be read from the
     .csv file.
+
+    Parameters
+    ----------
+    use_cache : bool, default True
+        If True the dataset will be retrieved from a csv file if one
+        exists, otherwise, it will be retrieved from the MySQL database. 
+        If False the dataset will be retrieved from the MySQL database
+        even if the csv file exists.
+
+    Returns
+    -------
+    DataFrame : A Pandas DataFrame containing the data from the titanic
+        dataset is returned.
     '''
 
-    if os.path.exists(titanic_file) and use_cache:
-        return pd.read_csv(titanic_file)
+    if os.path.exists(_titanic_file) and use_cache:
+        return pd.read_csv(_titanic_file)
     else:
-        df = pd.read_sql(_get_titanic_sql(), get_db_url(titanic_db))
-        df.to_csv(titanic_file, index = False)
+        df = pd.read_sql(_get_titanic_sql(), get_db_url(_titanic_db))
+        df.to_csv(_titanic_file, index = False)
         return df
 
 def _get_iris_sql() -> str:
+    '''
+    Returns the SQL code required to retrieve the iris dataset
+    from the MySQL database.
+    '''
+
     return '''
         SELECT *
         FROM measurements
@@ -55,16 +78,34 @@ def get_iris_data(use_cache = True) -> pd.core.frame.DataFrame:
     exist the data will be cached in that file inside the current
     working directory. Otherwise, the data will be read from the
     .csv file.
+
+    Parameters
+    ----------
+    use_cache: bool, default True
+        If True the dataset will be retrieved from a csv file if one
+        exists, otherwise, it will be retrieved from the MySQL database. 
+        If False the dataset will be retrieved from the MySQL database
+        even if the csv file exists.
+
+    Returns
+    -------
+    DataFrame : A Pandas DataFrame containing the data from the iris
+        dataset is returned.
     '''
 
-    if os.path.exists(iris_file) and use_cache:
-        return pd.read_csv(iris_file)
+    if os.path.exists(_iris_file) and use_cache:
+        return pd.read_csv(_iris_file)
     else:
-        df = pd.read_sql(_get_iris_sql(), get_db_url(iris_db))
-        df.to_csv(iris_file, index = False)
+        df = pd.read_sql(_get_iris_sql(), get_db_url_(iris_db))
+        df.to_csv(_iris_file, index = False)
         return df
 
 def _get_telco_sql() -> str:
+    '''
+    Returns the SQL code required to retrieve the telco dataset
+    from the MySQL database.
+    '''
+
     return '''
         SELECT *
         FROM customers
@@ -81,11 +122,24 @@ def get_telco_data(use_cache = True) -> pd.core.frame.DataFrame:
     exist the data will be cached in that file inside the current
     working directory. Otherwise, the data will be read from the
     .csv file.
+
+    Parameters
+    ----------
+    use_cache: bool, default True
+        If True the dataset will be retrieved from a csv file if one
+        exists, otherwise, it will be retrieved from the MySQL database. 
+        If False the dataset will be retrieved from the MySQL database
+        even if the csv file exists.
+
+    Returns
+    -------
+    DataFrame : A Pandas DataFrame containing the data from the telco
+        dataset is returned.
     '''
 
-    if os.path.exists(telco_file) and use_cache:
-        return pd.read_csv(telco_file)
+    if os.path.exists(_telco_file) and use_cache:
+        return pd.read_csv(_telco_file)
     else:
-        df = pd.read_sql(_get_telco_sql(), get_db_url(telco_db))
-        df.to_csv(telco_file, index = False)
+        df = pd.read_sql(_get_telco_sql(), get_db_url(_telco_db))
+        df.to_csv(_telco_file, index = False)
         return df
