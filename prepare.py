0a7e9888957e6fa138bd62d65f4848dbdd2cf5a4
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-03-01 14:18:57 -0600

    Minor formatting

--- prepare.py
+++ prepare.py
@@ -5,21 +5,21 @@ import pandas as pd
 
 def prep_iris_data(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     '''
-    Accepts the iris dataset and performs transformations to prepare the data 
-    for exploratory analysis.
+        Accepts the iris dataset and performs transformations to prepare the data 
+        for exploratory analysis.
 
-    Parameters
-    ----------
-    df : DataFrame
-        The expected argument is a Pandas DataFrame containing the iris
-        dataset.
+        Parameters
+        ----------
+        df : DataFrame
+            The expected argument is a Pandas DataFrame containing the iris
+            dataset.
 
-    Returns
-    -------
-    DataFrame : A Pandas DataFrame containing the iris dataset with transformations.
-        The resultant dataframe has the species_id and measurement_id columns
-        removed, the species_name column renamed to species, and an encoded variable
-        for the categorical column species.
+        Returns
+        -------
+        DataFrame : A Pandas DataFrame containing the iris dataset with transformations.
+            The resultant dataframe has the species_id and measurement_id columns
+            removed, the species_name column renamed to species, and an encoded variable
+            for the categorical column species.
     '''
 
     df = df.drop_duplicates()
@@ -34,21 +34,21 @@ def prep_iris_data(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
 
 def prep_titanic_data(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     '''
-    Accepts the titanic dataset and performs transformations to prepare the data
-    for exploratory analysis.
+        Accepts the titanic dataset and performs transformations to prepare the data
+        for exploratory analysis.
 
-    Parameters
-    ----------
-    df : DataFrame
-        The expected argument is a Pandas DataFrame containing the titanic
-        dataset.
+        Parameters
+        ----------
+        df : DataFrame
+            The expected argument is a Pandas DataFrame containing the titanic
+            dataset.
 
-    Returns
-    -------
-    DataFrame : A Pandas DataFrame containing the titanic dataset with transformations.
-        The resultant dataframe has the deck, embarked, class, and age columns removed,
-        null values in the embark_town column filled with the value 'Southampton', and
-        encoded variables for the categorical columns sex and embark_town.
+        Returns
+        -------
+        DataFrame : A Pandas DataFrame containing the titanic dataset with transformations.
+            The resultant dataframe has the deck, embarked, class, and age columns removed,
+            null values in the embark_town column filled with the value 'Southampton', and
+            encoded variables for the categorical columns sex and embark_town.
     '''
 
     df = df.drop_duplicates()
@@ -65,22 +65,22 @@ def prep_titanic_data(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
 
 def prep_telco_data(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     '''
-    Accepts the telco dataset and performs transformations to prepare the data
-    for exploratory analysis.
+        Accepts the telco dataset and performs transformations to prepare the data
+        for exploratory analysis.
 
-    Parameters
-    ----------
-    df : DataFrame
-        The expected argument is a Pandas DataFrame containing the telco
-        dataset.
+        Parameters
+        ----------
+        df : DataFrame
+            The expected argument is a Pandas DataFrame containing the telco
+            dataset.
 
-    Returns
-    -------
-    DataFrame : A Pandas DataFrame containing the telco dataset with transformations.
-        The resultant dataframe has the customer_id, contract_type_id, 
-        internet_service_type_id, and payment_type_id columns removed, and encoded
-        variables for all categorical columns (all non-numeric columns except customer_id
-        and total_charges). Rows with customers of 0 tenure are also removed.
+        Returns
+        -------
+        DataFrame : A Pandas DataFrame containing the telco dataset with transformations.
+            The resultant dataframe has the customer_id, contract_type_id, 
+            internet_service_type_id, and payment_type_id columns removed, and encoded
+            variables for all categorical columns (all non-numeric columns except customer_id
+            and total_charges). Rows with customers of 0 tenure are also removed.
     '''
 
     df = df.drop_duplicates()
